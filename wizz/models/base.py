650578a3b976412fca7b9375745dcb971666eada
Arthur Ryzhak <ryzhakar@gmail.com>
2024-03-17 22:30:56 +0200

    fix: async lazyloading in model relationships

--- wizz/models/base.py
+++ wizz/models/base.py
@@ -4,8 +4,10 @@ from sqlalchemy import DateTime
 from sqlalchemy.dialects import mysql
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.dialects import sqlite
+from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.sql import func
 # SQLAlchemy does not map BigInt to Int by default on the sqlite dialect.
 # It should, but it doesnt.
@@ -16,7 +18,7 @@ BigIntegerType = BigIntegerType.with_variant(mysql.BIGINT(), 'mysql')
 BigIntegerType = BigIntegerType.with_variant(sqlite.INTEGER(), 'sqlite')
 
 
-class CustomBase:
+class Base(AsyncAttrs, DeclarativeBase):
     """Base model class with ids and timestamps."""
 
     @declared_attr
@@ -39,6 +41,3 @@ class CustomBase:
         default=func.now(),
         onupdate=func.now(),
     )
-
-
-Base = declarative_base(cls=CustomBase)

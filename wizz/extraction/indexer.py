1af57af4be0c99e4c9e9d2b5e5004c0b365185a9
Arthur Ryzhak <ryzhakar@gmail.com>
2024-03-17 19:16:09 +0200

    fix: indexer initialization

--- wizz/extraction/indexer.py
+++ wizz/extraction/indexer.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
+import os
 from functools import lru_cache
-from os import path
 from typing import Self
 
 import numpy as np
 from aiorwlock import RWLock
 from annoy import AnnoyIndex
 from numpy import ndarray
+from numpy import zeros
 
 from wizz.extraction import constants
 
@@ -15,7 +16,11 @@ from wizz.extraction import constants
 class AnnoyIndexManager:
     """Manages access to hard disk-based Annoy indices."""
 
-    def __init__(self, user_id: str):
+    def __init__(
+        self,
+        user_id: str,
+        read_mode: bool = True,
+    ):
         """Initialize a user-specific Annoy index."""
         self.user_id = user_id
         self.index = AnnoyIndex(
@@ -24,15 +29,25 @@ class AnnoyIndexManager:
         )
         self.lock = RWLock()
         self.refresh_index_connection()
+        self.read_mode = read_mode
 
     def refresh_index_connection(self) -> None:
         """Load the index memory-mapping from disk."""
-        self.index.load(f'{self.user_id}.ann')
+        if not os.path.exists(constants.ANNOY_INDICES_STORE_PATH):
+            os.makedirs(constants.ANNOY_INDICES_STORE_PATH)
+        if not os.path.exists(self.path):
+            self.index.add_item(
+                0,
+                zeros(constants.EMBEDDING_DIM, dtype=constants.DTYPE),
+            )
+            self.index.build(10)
+            self.index.save(self.path)
+        self.index.load(self.path)
 
     @property
     def path(self) -> str:
         """Get the path to the user-specific Annoy index."""
-        return path.join(
+        return os.path.join(
             constants.ANNOY_INDICES_STORE_PATH,
             f'{self.user_id}.ann',
         )
@@ -47,9 +62,9 @@ def get_index_manager(user_id):
 class AnnoyReader:
     """A context manager that allows read access to an Annoy index."""
 
-    def __init__(self, user_id: str):
+    def __init__(self, index_name: str):
         """Initialize the index manager."""
-        self.manager = get_index_manager(user_id)
+        self.manager = get_index_manager(index_name)
 
     async def __aenter__(self) -> Self:
         """Acquire a read lock."""
@@ -80,13 +95,14 @@ class AnnoyReader:
 class AnnoyWriter:
     """A context manager that allows write access to an Annoy index."""
 
-    def __init__(self, user_id: str):
+    def __init__(self, index_name: str):
         """Initialize the index manager."""
-        self.manager = get_index_manager(user_id)
+        self.manager = get_index_manager(index_name)
 
     async def __aenter__(self) -> Self:
         """Acquire a write lock."""
         await self.manager.lock.writer_lock.acquire()
+        self.manager.index.unload()
         return self
 
     async def __aexit__(self, exc_type, exc, tb):

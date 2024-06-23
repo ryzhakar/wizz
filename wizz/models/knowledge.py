efe174fd33159fb59122abb55f3031381c688368
Arthur Ryzhak <ryzhakar@gmail.com>
2024-06-24 00:02:59 +0300

    refactor: refine models and start migrations from scratch

--- wizz/models/knowledge.py
+++ wizz/models/knowledge.py
@@ -1,8 +1,6 @@
-from sqlalchemy import Column
-from sqlalchemy import Float
 from sqlalchemy import ForeignKey
-from sqlalchemy import Integer
-from sqlalchemy import String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 
 from wizz.models.base import Base
@@ -10,40 +8,84 @@ from wizz.models.base import Base
 
 class Context(Base):
     """A space for shared knowledge."""
-    name = Column(String, nullable=False, unique=True, index=True)
 
-    sources = relationship('Source', back_populates='context')
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(
+        nullable=False,
+        unique=True,
+        index=True,
+    )
+
+    sources: Mapped[list['Source']] = relationship(
+        'Source',
+        back_populates='context',
+        cascade='all, delete-orphan',
+    )
 
 
 class Source(Base):
     """A metadata about a source text."""
-    context_id = Column(Integer, ForeignKey('context.id'), nullable=False)
-    name = Column(String, nullable=False)
-    hash = Column(String, nullable=False)
-    vector_hex = Column(String, nullable=False)
 
-    context = relationship('Context', back_populates='sources')
-    blobs = relationship('Blob', back_populates='source')
-    links = relationship('Link', back_populates='target_source')
+    id: Mapped[int] = mapped_column(primary_key=True)
+    context_id: Mapped[int] = mapped_column(
+        ForeignKey('context.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+    name: Mapped[str] = mapped_column(nullable=False)
+    hash: Mapped[str] = mapped_column(nullable=False)
+    vector_hex: Mapped[str] = mapped_column(nullable=False)
+
+    context: Mapped['Context'] = relationship(
+        'Context', back_populates='sources',
+    )
+    blobs: Mapped[list['Blob']] = relationship(
+        'Blob',
+        back_populates='source',
+        cascade='all, delete-orphan',
+    )
+    links: Mapped[list['Link']] = relationship(
+        'Link',
+        back_populates='target_source',
+        cascade='all, delete-orphan',
+    )
 
 
 class Blob(Base):
     """A vectorized source text section."""
-    source_id = Column(Integer, ForeignKey('source.id'), nullable=False)
-    text = Column(String, nullable=False)
-    blob_index = Column(Integer, nullable=False)
-    vector_hex = Column(String, nullable=False)
 
-    source = relationship('Source', back_populates='blobs')
-    links = relationship('Link', back_populates='blob')
+    id: Mapped[int] = mapped_column(primary_key=True)
+    source_id: Mapped[int] = mapped_column(
+        ForeignKey('source.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+    text: Mapped[str] = mapped_column(nullable=False)
+    blob_index: Mapped[int] = mapped_column(nullable=False)
+    vector_hex: Mapped[str] = mapped_column(nullable=False)
+
+    source: Mapped['Source'] = relationship('Source', back_populates='blobs')
+    links: Mapped[list['Link']] = relationship(
+        'Link',
+        back_populates='blob',
+        cascade='all, delete-orphan',
+    )
 
 
 class Link(Base):
     """A blob-based inter-source relationship."""
-    blob_id = Column(Integer, ForeignKey('blob.id'), nullable=False)
-    target_source_id = Column(Integer, ForeignKey('source.id'), nullable=False)
-    origin_distance = Column(Float, nullable=False)
-    destination_distance = Column(Float, nullable=False)
 
-    blob = relationship('Blob', back_populates='links')
-    target_source = relationship('Source', back_populates='links')
+    id: Mapped[int] = mapped_column(primary_key=True)
+    blob_id: Mapped[int] = mapped_column(
+        ForeignKey('blob.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+    target_source_id: Mapped[int] = mapped_column(
+        ForeignKey('source.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+    origin_distance: Mapped[float] = mapped_column(nullable=False)
+    destination_distance: Mapped[float] = mapped_column(nullable=False)
+
+    blob: Mapped['Blob'] = relationship('Blob', back_populates='links')
+    target_source: Mapped['Source'] = relationship(
+        'Source', back_populates='links',
+    )
